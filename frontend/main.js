60556205904c5c9d34f2c3d2e04526d3bf962f13
Muba-01 <muhammadmbs99x@gmail.com>
2025-07-17 19:04:39 +0800

    initial commit

---
+++ frontend/main.js
@@ -0,0 +1,16 @@
+
+
+const circle = document.querySelector('.circle');
+const sidebar = document.querySelector('.side-navbar');
+// const sidebarLinks = document.querySelectorAll('.side-navbar ul li a');
+
+
+    circle.addEventListener('click', () => {
+    sidebar.classList.toggle('closed');
+    circle.classList.toggle('closed');
+
+    const icon = circle.querySelector('i');
+    icon.classList.toggle('fa-angle-right');
+    icon.classList.toggle('fa-angle-left');
+
+    });
